5a15401534e4718e7736f92cba3353efbf956485
ZZZmaverick <1193391807@qq.com>
2021-03-11 21:21:55 +0800

    v3.2

--- src/store.js
+++ src/store.js
@@ -6,8 +6,30 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     isLogin: false,
-    userData: {}
+    userData: {
+      username: "",
+      password: "",
+      nickname: "",
+      gender: 0,
+      avatar: 0,
+      money: 0,
+      health: 0,
+      faith: 0,
+      skills: [],
+      equipments: [],
+      cards: [],
+      backupSki: [],
+      backupEqu: [],
+      backupCards: []
+    }
   },
-  mutations: {},
+  mutations: {
+    changeLoginState(state) {
+      state.isLogin = !state.isLogin;
+    },
+    chageUserData(state, newData) {
+      state.userData = newData;
+    }
+  },
   actions: {}
 });
